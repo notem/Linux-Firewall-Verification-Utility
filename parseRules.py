0f6ade46bb2a8602636ba4efe8d244c2ba801f13
Michael-Rhodes <mjr2563@rit.edu>
2018-04-17 10:04:17 -0700

    documentation, formatted output

--- parseRules.py
+++ parseRules.py
@@ -1,28 +1,47 @@
 #!/usr/bin/env python3
 
-# date: 2018-04-09
+# date: 2018-04-17
 # contributor(s):
 #	Michael Rhodes, mjr2563@rit.edu
 # description:
-#	
-# usage:
-#
+#	Command-line python tool to audit firewall policies in iptables. This 
+#	tool imports a python module called firewall_verifier. Steps to install
+#	this module can be found at:
+#		https://github.com/notem/Linux-Firewall-Verification-Utility
 
+usageStatement = """	
+Usage: fverify [policy] -file [filename]
+	Specifying a policy: 
+		use the same options you would use to create a rule in iptables
+	Required parameters:
+		-j | --jump [target]	specify the target (ADD/DROP)
+		-A | --append [chain]	specify the chain to look at
+		-file [filename]	file containing iptables-save output
+	Optional parameters:
+		source/destination ports (e.g. --dports, --source-port)
+		source/destination addresses (e.g. -s, -d, --src-range)
+		protocol(s) (e.g. -p, --protocols)
+		
+Limitations:
+	- This tool currently only looks at the options specified in the
+	  required and optional parameters. For instance, it ignores options
+	  from most extension modules, such as matching TCP connections and 
+	  filtering by MAC address. More options can be added by adding more
+	  tuples to the algorithm.
+	- This tool currently doesn't work with rules that don't jump to a 
+	  target. For example, it doesn't work with rules that use -g or --goto
+	  instead of -j or --jump
+"""
 
 import firewall_verifier as fv
 from ipaddress import IPv4Address,IPv4Network
 from socket import getprotobyname
-# from subprocess import check_output
 from sys import argv,exit
 from os import path
 
 
 # TODO: Check if algorithm can handle more than 1/0 for the policy
-#	clean up comments, create usage statement
-#	print more information when a witness packet is found
-
-
-#possible problems:
+#	print more information when a witness packet is found such as the rule string
 #	how should we handle rules with a goto [chain] instead of jump [target]?
 
 
@@ -43,7 +62,7 @@ targets = ['-j','--jump']
 #### ports ####
 # --destination-port, --dport, --sport, --source-port, --dports, --sports
 # possible formats: 80   80,443  1000:1010   1:900,1000:10000
-#	ports can be negated with "!" ("! --dports 0:999")
+# TODO	ports can be negated with "!" ("! --dports 0:999")
 dports = ['--destination-port', '--dport', '--dports']
 sports = ['--sport', '--source-port', '--sports']
 
@@ -51,7 +70,7 @@ sports = ['--sport', '--source-port', '--sports']
 # -d, --destination, -dst, -s, --source, -src
 # -possible formats: 1.1.1.0/24, 1.1.1.1, 1.1.1.0/255.255.255.0
 # -src-range (-dst-range) 1.1.1.20-1.1.1.23
-# 	addresses can be negated with !
+# TODO 	addresses can be negated with !
 saddresses = ['-s', '--source', '-src', '--src-range']
 daddresses = ['-d', '--destination', '-dst', '--dst-range']
 
@@ -68,12 +87,14 @@ protocols = ['-p', '--protocols']
 
 ### Usage Statement
 def usage():
-  exit(1)
+  exit(usageStatement)
 
 ### parseRule
 # returns a LIST containing the tuples derived from the rule
 #	a list is used because a single rule might expand into several
-# assumes chain has already been checked
+# 
+# rule - a string containing the iptable options to specify the rule
+#	 For example: "-A INPUT -p ipencap -j DROP"
 def parseRule(rule):
   rule = rule.rstrip('\r\n').split(" ")
  # min/max values for each tuple
@@ -89,7 +110,7 @@ def parseRule(rule):
   protoMax = 255
   rules = [] 		# stores all created rules, returned on exit
   
- # iterate of rule to extract ranges
+ # iterate over rule to extract ranges
   i = 0
   while i < len(rule):
 
@@ -132,14 +153,18 @@ def parseRule(rule):
       continue
 
    # check for protocol
+   # TODO
     if any(j == rule[i] for j in protocols):
       i = i + 2
       try:
-#        if (rule[i-1] == "all"): continue
-#		-p all can be used to create rules but its omitted in iptables-save
+        if (rule[i-1] == "all"): continue
         p = int(rule[i-1])
       except:
-        p = getprotobyname(rule[i-1])
+        try:
+          p = getprotobyname(rule[i-1])
+        except Exception as e:
+          print("ERROR: "+str(e))
+          usage()
       protoMin = protoMax = p
       continue
 
@@ -201,7 +226,8 @@ def parseRule(rule):
 ### extractRules
 # given a file and a chain, it parses each line and adds rules that correspond to
 # the given chain to the firewall verification object
-# f - file pointer
+#
+# f - file pointer (created after opening the file)
 # chain - name of the chain to look at
 def extractRules(f, chain):
   defaultPolicy = ""
@@ -211,14 +237,13 @@ def extractRules(f, chain):
         rules = parseRule(i)	# returns a list of rule tuples
         for r in rules:
           fv.add(r)		# add rules to verifier
-          print("DEBUG: ",r)
     elif (i[0] == ':'): # line defines a chain
       if (":"+chain in i.split(' ')): 
         defaultPolicy = i.split(" ")[1] 
         if (defaultPolicy=='ACCEPT'): defaultPolicy = 1  
         else: defaultPolicy=0
   if (defaultPolicy == ""):
-    print("Can't find a default policy for '"+chain+"'")
+    print("ERROR: Can't find a default policy for '"+chain+"'")
     usage()
   fv.add(((0,4294967295),(1,65535),(0,4294967295),(1,65535),(0,255),defaultPolicy))
 
@@ -229,7 +254,7 @@ ruleFile = ""
 chain = ""
 targetPresent = False
 argv = argv[1:]
-for i in range(len(argv)):
+for i in range(len(argv)):	# check for required parameters
   if (any(j == argv[i] for j in chains)):	# check for chain
     chain = argv[i+1]
   if (any(j == argv[i] for j in fileArgs)):	# check for file
@@ -247,7 +272,7 @@ if (not targetPresent):
   usage()
 
 if (not path.exists(ruleFile)):			# check if file exists
-  print("Cannot find the file : "+ruleFile)
+  print("ERROR: Cannot find the file "+ruleFile)
   usage()
 
 ### parse file, create tuples, and check for witness
