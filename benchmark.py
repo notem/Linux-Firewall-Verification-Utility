0ee15401041ee2f62112f0106b08f263c66698f7
notem <njm3308@rit.edu>
2018-04-25 16:29:23 -0400

    change: minor adjustments to benchmarking script to get some slightly more useful data for the report

--- benchmark.py
+++ benchmark.py
@@ -6,26 +6,27 @@ import random as r
 from timeit import default_timer as timer
 
 # tweak-able parameters
-trials = 1000
+trials = 100
 rules = 10**5
+max_rule_type = 2
 
 
 # randomly generate a 2-tuple, representing the range of the field
 def generate_tuple():
-    lo = r.randint(1, 1 << 32)
+    lo = r.randint(1, 1 << 31)
     hi = r.randint(lo, 1 << 32)
     tup = (lo, hi)
     return tup
 
 
-# generate a tuple representing a firewall rule
+# generate a tuple-of-tuples representing a firewall rule
 def generate_rule():
     tup1 = generate_tuple()
     tup2 = generate_tuple()
     tup3 = generate_tuple()
     tup4 = generate_tuple()
     tup5 = generate_tuple()
-    rule = (tup1, tup2, tup3, tup4, tup5, r.randint(0, 1))
+    rule = (tup1, tup2, tup3, tup4, tup5, r.randint(0, max_rule_type))
     return rule
 
 
@@ -46,9 +47,17 @@ def main():
     end = timer()
     print(end-start, "seconds")
 
-    # dry run (no cartesian combinations generated)
+    # dry run (no cartesian combinations generated, however all slices generated)
     prop = ((0, 0), (0, 0), (0, 0), (0, 0), (0, 0), 0)
     print("\n[dry run]: ", end="")
+    start = timer()
+    fv.verify(prop)
+    end = timer()
+    print(end-start, "seconds")
+
+    # fat run (fails on the first non-matching rule)
+    prop = ((0, 1 << 32-1), (0, 1 << 32-1), (0, 1 << 32-1), (0, 1 << 32-1), (0, 1 << 32-1), 0)
+    print("[fat run]: ", end="")
     start = timer()
     fv.verify(prop)
     end = timer()
@@ -57,27 +66,33 @@ def main():
     # try random properties and compute an average
     print("\n[trials]: ")
     times = []
+    rates = {True: 0, False: 0}
     for i in range(0, trials):
         prop = generate_rule()
-        print("\t[", i+1, "]: ", end="")
+        print("\t[", (i+1), "]:\t", end="")
         start = timer()
-        fv.verify(prop)
+        suc = fv.verify(prop)
         end = timer()
+        rates[suc] += 1
         times.append(end-start)
         print(times[i], "seconds")
+
+    # percentage of random properties which verified successfully
+    print("\n[rate]:\t", rates[True]/trials)
 
     # find max time taken
     time_max = 0
     for time in times:
-        if time_max > time:
+        if time > time_max:
             time_max = time
+    print("[max]:\t", time_max, "seconds")
+
     # calculate average
     avg = 0
     for time in times:
         avg += time
     avg /= trials
-    print("\n[max]: ", time_max, "seconds")
-    print("\n[average]: ", avg, "seconds")
+    print("[avg]:\t", avg, "seconds")
     return
 
 
